75afaff97104a62e5d8f749bf6fa46730022156f
Andre Fonseca <andrefonseca@neuro.ufrn.br>
2021-08-13 11:57:20 -0500

    Commited Fri 13 Aug 2021 11:57:20 AM CDT @ pop-os

--- R/cross_helpers.R
+++ R/cross_helpers.R
@@ -1,19 +1,12 @@
-#' The length of a string
+#' Title
+#' @description
 #'
-#' Technically this returns the number of "code points", in a string. One
-#' code point usually corresponds to one character, but not always. For example,
-#' an u with a umlaut might be represented as a single character or as the
-#' combination a u and an umlaut.
-#'
-#' @inheritParams str_detect
-#' @return A numeric vector giving number of characters (code points) in each
-#'    element of the character vector. Missing string have missing length.
-#' @seealso [stringi::stri_length()] which this function wraps.
+#' @return
 #' @export
+#'
 #' @examples
-#' str_length(letters)
-#' str_length(NA)
-#' str_length(factor("abc"))
-#' str_length(c("i", "like", "programming", NA))
-str_length <- function(string) {
+#' cross_output %>% cross_ranking_plot()
+
+cross_ranking_plot <- function() {
+  print("Something")
 }
